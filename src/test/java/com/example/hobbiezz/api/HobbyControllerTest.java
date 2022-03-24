4d9b5747529b0d5aaa7bb0c92cd6553a1e5e7a54
amandatolstruplaursen <amanda.t.laursen@hotmail.com>
2022-03-24 19:12:45 +0100

    Tests tilføjet

---
+++ src/test/java/com/example/hobbiezz/api/HobbyControllerTest.java
@@ -0,0 +1,95 @@
+package com.example.hobbiezz.api;
+
+import com.example.hobbiezz.entity.Hobby;
+import com.example.hobbiezz.repository.AddressRepository;
+import com.example.hobbiezz.repository.HobbyInfoRepository;
+import com.example.hobbiezz.repository.HobbyRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+class HobbyControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+
+    @Autowired
+    HobbyRepository hobbyRepository;
+
+    @Autowired
+    AddressRepository addressRepository;
+
+    @Autowired
+    HobbyInfoRepository hobbyInfoRepository;
+
+    /*
+    @Autowired
+    PersonRepository personRepository;
+
+    @Autowired
+    HobbyInfoRepository hobbyInfoRepository;
+
+     */
+
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+
+    @BeforeEach
+    public void setup() {
+        hobbyInfoRepository.deleteAll();
+        hobbyRepository.deleteAll();
+        Hobby hobbyOne = hobbyRepository.save(new Hobby("Name1", "Category1", "Link1.dk", "outside"));
+        Hobby hobbyTwo = hobbyRepository.save(new Hobby("Name2", "Category2", "Link2.dk", "inside"));
+
+    }
+
+    /*
+    //Virker ikke 22/3
+    @Test
+    void testGetHobbies() throws Exception {
+        String name = "$[?(@.name == '%s')]";
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/api/hobbies/all")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0]").exists())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.length()").value(2))
+
+                //Testing
+                .andExpect(MockMvcResultMatchers.jsonPath(name, "Name1").exists())
+                .andExpect(MockMvcResultMatchers.jsonPath(name, "Name2").exists());
+
+    }
+
+
+    //Virker ikke 22/3
+    @Test
+    void testGetHobby() throws Exception {
+        mockMvc.perform( MockMvcRequestBuilders
+                        .get("/api/hobbies/{name}", "Name1")
+                        .accept(MediaType.APPLICATION_JSON))
+                        .andDo(print())
+                        .andExpect(status().isOk())
+                        .andExpect(MockMvcResultMatchers.jsonPath("$.name").value("Name1"));
+    }
+
+
+    @Test
+    void testGetPersonsHobbies() {
+
+    }
+
+     */
+}
---
+++ src/test/java/com/example/hobbiezz/api/HobbyInfoControllerTest.java
@@ -0,0 +1,133 @@
+package com.example.hobbiezz.api;
+
+import com.example.hobbiezz.entity.Hobby;
+import com.example.hobbiezz.entity.HobbyInfo;
+import com.example.hobbiezz.entity.Person;
+import com.example.hobbiezz.repository.AddressRepository;
+import com.example.hobbiezz.repository.HobbyInfoRepository;
+import com.example.hobbiezz.repository.HobbyRepository;
+import com.example.hobbiezz.repository.PersonRepository;
+import com.example.hobbiezz.service.HobbyInfoService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDateTime;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+class HobbyInfoControllerTest {
+
+    //Testene viser, at metoderne ikke virker, 22/3
+
+    @Autowired
+    MockMvc mockMvc;
+
+
+    @Autowired
+    HobbyInfoRepository hobbyInfoRepository;
+
+    @Autowired
+    HobbyRepository hobbyRepository;
+
+    @Autowired
+    PersonRepository personRepository;
+
+    @Autowired
+    AddressRepository addressRepository;
+
+    HobbyInfoService hobbyInfoService;
+
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    static int hobbyInfoOneId, hobbyInfoTwoId;
+    static int personId;
+
+    /*
+    public HobbyInfoControllerTest(HobbyInfoRepository hobbyInfoRepository, HobbyRepository hobbyRepository,
+    PersonRepository personRepository) {
+        this.hobbyInfoRepository = hobbyInfoRepository;
+        this.hobbyRepository = hobbyRepository;
+        this.personRepository = personRepository;
+    }
+
+     */
+
+
+    @BeforeEach
+    public void setup() throws Exception {
+        addressRepository.deleteAll();
+        hobbyInfoRepository.deleteAll();
+        hobbyRepository.deleteAll();
+        personRepository.deleteAll();
+
+        //Person
+        Person personOne = personRepository.save
+                (new Person("Isabel@mail.dk", "Isabel", "Isabelsen", "911"));
+        Person personTwo = personRepository.save
+                (new Person("Andrea@mail.dk", "Andrea", "Andreasen", "88888888"));
+
+
+        //Hobby
+        Hobby hobbyOne = hobbyRepository.save
+                (new Hobby("Name", "Category", "Link.dk", "out"));
+
+        //Hobbyinfo
+
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime later = LocalDateTime.now().minusDays(100);
+        hobbyInfoOneId = hobbyInfoRepository.save(new HobbyInfo(now, hobbyOne, personOne)).getId();
+        hobbyInfoOneId = hobbyInfoRepository.save(new HobbyInfo(later, hobbyOne, personTwo)).getId();
+        System.out.println("-----------Setup completed-----------");
+    }
+
+    /*
+    //Virker ikke 22/3
+    @Test
+    void testDeleteHobbyInfo() throws Exception {
+        this.mockMvc.perform(MockMvcRequestBuilders
+                        .delete("/api/personalHobbies/{id}", hobbyInfoOneId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                        .andExpect(status().isOk());
+
+        //Testing
+        assertEquals(1, hobbyInfoRepository.count());
+
+    }
+
+
+    @Test
+    void testGetHObby() throws Exception {
+        mockMvc.perform( MockMvcRequestBuilders
+                        .get("/api/personalHobbies/{name}", hobbyInfoOneId)
+                        .accept(MediaType.APPLICATION_JSON))
+                        .andDo(print())
+                        .andExpect(status().isOk())
+                        .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(hobbyInfoOneId));
+    }
+
+
+
+    //Virker ikke
+    @Test
+    void getHobbyInfo() throws Exception {
+
+        mockMvc.perform( MockMvcRequestBuilders
+                        .get("/api/personalHobbies/{id}", hobbyInfoOneId)
+                        .accept(MediaType.APPLICATION_JSON))
+                        .andDo(print())
+                        .andExpect(status().isOk())
+                        .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(hobbyInfoOneId));
+
+    }
+
+     */
+}
