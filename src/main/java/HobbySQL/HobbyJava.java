f1b90c25325f7bf7650731b02b6bb7b4556567ee
andreajiao <andreajiao@gmail.com>
2022-03-28 08:34:12 +0200

    ændret path i HobbyJava, så den matcher dette projekts struktur

--- src/main/java/HobbySQL/HobbyJava.java
+++ src/main/java/HobbySQL/HobbyJava.java
@@ -12,7 +12,7 @@ public class HobbyJava {
         String createTableHobby="";
         String createWithInsert = null;
 
-        createWithInsert =createTableHobby + Files.lines(Paths.get("Hobby.txt"))
+        createWithInsert =createTableHobby + Files.lines(Paths.get("src/main/java/HobbySQL/Hobbies.text"))
                 .map(hobby -> {
                     String[] c= hobby.split(";");
                     return String.format("hobbyRepository.save(new Hobby(\"%s\",\"%s\",\"%s\",\"%s\")),",c[0],c[1],c[2],c[3]);
